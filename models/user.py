d3c281abcc3c5b4220556ee1fc5f55371002b11b
aquimo <aquimoadriano@gmail.com>
2024-02-20 14:13:58 +0200

    Models

---
+++ models/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""User class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+
+
+class User(BaseModel, Base):
+    """Class for user
+    Attributes:
+        email: Email address
+        password: Password for you login
+        first_name: First name
+        last_name: Last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
